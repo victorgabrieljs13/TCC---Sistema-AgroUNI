65319095634d75f95eb06d7f1e872917daa34e5f
Victor Gabriel Santos <victorgabrieljs13@gmail.com>
2026-08-30 10:52:23 -0300

    Corrige ordem das rotas do servidor

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -16,9 +16,6 @@ app.use(express.static(path.join(__dirname, '..', 'frontend')));
 const feirantesRoutes = require('./routes/feirantesRoutes');
 app.use('/feirantes', feirantesRoutes);
 
-app.get('/', (req, res) => {
-    res.json({ mensagem: 'API do sistema de feirantes rodando!' });
-});
 
 const produtosRoutes = require('./routes/produtosRoutes');
 app.use('/produtos', produtosRoutes);
